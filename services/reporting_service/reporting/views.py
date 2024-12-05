96ca4b5dbf593165d8f0de3576b7651424178fba
sarahwonago <sarahshaaleh@gmail.com>
2024-12-05 12:29:02 +0300

    report viewset

--- services/reporting_service/reporting/views.py
+++ services/reporting_service/reporting/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+from .models import Report
+from .serializers import ReportSerializer
+
+class ReportViewSet(viewsets.ModelViewSet):
+    """
+    Viewset for managing wildlife reports.
+    """
+
+    queryset = Report.objects.all()
+    serializer_class = ReportSerializer
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+    filterset_fields = [
+        "status",
+        "animal_type"
+    ]
+    search_fields = [
+        "description",
+        "animal_type"
+    ]
+    ordering_fields = [
+        "created_at",
+        "updated_at"
+    ]
+    ordering = [
+        "-created_at"
+    ]
