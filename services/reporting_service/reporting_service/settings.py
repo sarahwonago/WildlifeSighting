8f33c8cca158d4efb94bb2eec43f6e011d2bb7ef
sarahwonago <sarahshaaleh@gmail.com>
2024-12-05 12:43:43 +0300

    url patterns

--- services/reporting_service/reporting_service/settings.py
+++ services/reporting_service/reporting_service/settings.py
@@ -132,3 +132,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
