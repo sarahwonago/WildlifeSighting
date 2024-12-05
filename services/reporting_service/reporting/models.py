2d775c67df11acc5d498a0962d8f264c41715b2a
sarahwonago <sarahshaaleh@gmail.com>
2024-12-05 12:21:11 +0300

    Animal sight report model

--- services/reporting_service/reporting/models.py
+++ services/reporting_service/reporting/models.py
@@ -1,3 +1,37 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Report(models.Model):
+    """
+    Model representing a wildlife sighting report.
+    """
+
+    REPORT_STATUS = [
+        ("Pending", "Pending"),
+        ("Resolved", "Resolved"),
+        ("Escalated", "Escalated")
+    ]
+
+    id = models.UUIDField(
+        primary_key=True,
+        editable=False,
+        default=uuid.uuid4
+    )
+    animal_type = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    photo_url = models.URLField(
+        blank=True,
+        null=True
+    )
+    status = models.CharField(
+        max_length=50,
+        choices=REPORT_STATUS,
+        default="Pending"
+        
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.animal_type} sighted at {self.location}"
--- services/reporting_service/reporting_service/settings.py
+++ services/reporting_service/reporting_service/settings.py
@@ -73,23 +73,23 @@ WSGI_APPLICATION = "reporting_service.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config("DATABASE_PASSWORD"),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DATABASE_NAME'),
+#         'USER': config('DATABASE_USER'),
+#         'PASSWORD': config("DATABASE_PASSWORD"),
+#         'HOST': config('DATABASE_HOST'),
+#         'PORT': config('DATABASE_PORT'),
+#     }
+# }
 
 
 # Password validation
